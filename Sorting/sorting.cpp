8021967a508a4629976d085b3538bd7a7fc18bed
Abhishek Pathak <abhishekkumarpathak@Abhisheks-MacBook-Air.local>
2024-02-10 23:35:40 +0530

    Added

---
+++ Sorting/sorting.cpp
@@ -0,0 +1,21 @@
+/* Selection Sort*/
+#include<iostream>
+using namespace std;
+void swap(int *x ,int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+}
+void selectionSort(int arr[], int n){
+    int i , j , min_idx;
+    for(int  i = 0 ; i <n-1 ;i++){
+         
+    }
+
+}
+int main(){
+    int arr[5];
+
+
+}
