2feb2a295e2735f85fc562959dfbf66564d6d1fc
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-02-12 23:10:34 +0530

    updated

--- Proj/hello.java
+++ Proj/hello.java
@@ -1,8 +1,25 @@
 package Proj;
 
+import java.util.Scanner;
+
+public class Student{
+    String name ;
+    int age ;
+    int marks;
+}
 public class hello{
+
     public static void main(String args[]){
-        System.out.println("hello java");
+        Scanner inp = new Scanner(System.in);
+        Student stud[] = new Student[2];
+        
+        for(Student stu:stud){
+            String stud[].name = inp.nextLine();
+        }
+        for(int n =0 ;n<stud.length;n++){
+       System.out.println(stud[n].name);
+        }
 
     }
+    //
 }
