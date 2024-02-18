16d4d522045d8abb50c5c66146207e7f01f40110
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-02-18 15:35:44 +0530

    comp

--- Proj/BubbleSort.java
+++ Proj/BubbleSort.java
@@ -2,10 +2,9 @@ package Proj;
 
 import java.util.Scanner;
 
-public class SelectionSort extends Sort{
- 
-    public static void main(String[] args){
-           Scanner inp = new Scanner(System.in);
+public class BubbleSort extends Sort{
+public static void main(String[] args){
+         Scanner inp = new Scanner(System.in);
         System.out.println("Size of array :");
         int size = inp.nextInt();
         int[] array = init_array(size);
@@ -14,27 +13,29 @@ public class SelectionSort extends Sort{
         long startTime = System.nanoTime();
         int nSwap = 0;
         int nComp = 0;
-        for(int i = 0 ; i<array.length;i++){
-     
-            for(int j = i+1 ; j< array.length;j++){
-                if(array[j]<array[i]){
-                   swap(array, j, i);
-               
+
+        for(int i=size-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                if(array[j]>array[j+1]){
+                   swap(array, j, j+1);
+                   nSwap++;
+                   System.out.print("Swapped " +  i + " with " +j+"\t" );
                 }
                 nComp++;
                 System.out.println("Compared "+ i + " with " +j);
                 print_array(array);
             }
+
         }
         long stopTime = System.nanoTime();
         double duration = stopTime-startTime/1_000_000_000;
-        System.out.println("\n Array after Selection Sort :");
+        System.out.println("\n Array after Bubble Sort :");
     
         print_array(array);
         System.out.println("Time elapsed   :  " + duration);
         System.out.println("Total comparisons   :  " + nComp);
         System.out.println("Total swaps :  " + nSwap);
 
-    }
-
+}
+    
 }
--- Proj/InsertionSort.java
+++ Proj/InsertionSort.java
@@ -2,10 +2,9 @@ package Proj;
 
 import java.util.Scanner;
 
-public class SelectionSort extends Sort{
- 
-    public static void main(String[] args){
-           Scanner inp = new Scanner(System.in);
+public class InsertionSort extends Sort {
+   public static void main(String[] args){
+         Scanner inp = new Scanner(System.in);
         System.out.println("Size of array :");
         int size = inp.nextInt();
         int[] array = init_array(size);
@@ -14,27 +13,34 @@ public class SelectionSort extends Sort{
         long startTime = System.nanoTime();
         int nSwap = 0;
         int nComp = 0;
-        for(int i = 0 ; i<array.length;i++){
-     
-            for(int j = i+1 ; j< array.length;j++){
-                if(array[j]<array[i]){
-                   swap(array, j, i);
-               
+
+        for(int i =0 ; i < size - 1 ; i++){
+
+
+            for(int j=i+1 ;j > 0 ;j--){
+
+
+                if(array[j]<array[j-1]){
+                   swap(array, j, j-1);
+
+
+
+                   nSwap++;
+                   System.out.print("Swapped " +  (j-1) + " with " +j+"\t" );
                 }
                 nComp++;
                 System.out.println("Compared "+ i + " with " +j);
                 print_array(array);
             }
+
         }
         long stopTime = System.nanoTime();
         double duration = stopTime-startTime/1_000_000_000;
-        System.out.println("\n Array after Selection Sort :");
+        System.out.println("\n Array after Insertion Sort :");
     
         print_array(array);
         System.out.println("Time elapsed   :  " + duration);
         System.out.println("Total comparisons   :  " + nComp);
-        System.out.println("Total swaps :  " + nSwap);
-
-    }
-
+        System.out.println("Total swaps :  " + nSwap); 
+}
 }
