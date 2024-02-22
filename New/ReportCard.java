dc4575b7c9b367f71d6ea82b4de05ee9948160f3
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-02-22 23:33:12 +0530

    up

--- New/ReportCard.java
+++ New/ReportCard.java
@@ -1,6 +1,16 @@
 package New;
 
 public class ReportCard{
-    public static void main(String[] args) {  
-JF
+    String subject;
+    int Standard;
+    static  String SchoolName = "DAV Public School";
+    public void ReportCard(){
+        System.out.print("In Class");
+    }
+    public static void main(String[] args) { 
+        Class.forName("ReportCard"); 
+     ReportCard card1= new ReportCard();
+     System.out.println(card1.SchoolName);
+
+}
 }
