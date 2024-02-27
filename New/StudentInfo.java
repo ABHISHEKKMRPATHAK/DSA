d481600c8b064d5b89188312e0e2f2e77320469a
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-02-27 22:48:57 +0530

    updated

--- New/StudentInfo.java
+++ New/StudentInfo.java
@@ -9,8 +9,6 @@ public class StudentInfo{
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
-    public int getRollNumber() {
-        return rollNumber;
-    }
+
    
 }
